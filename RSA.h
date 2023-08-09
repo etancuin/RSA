a81c13135859b6907a60898bee083e7458dbc999
etancuin <114794458+etancuin@users.noreply.github.com>
2023-08-08 20:42:50 -0700

    Update RSA.h

--- RSA.h
+++ RSA.h
@@ -2,6 +2,9 @@
 #include <cmath>
 #include <climits>
 #include <cstdlib>
+#include <string>
+#include <sstream>
+
 using namespace std;
 
 #ifndef RSA_H
@@ -12,6 +15,8 @@ unsigned long long getPrime(unsigned long long min, unsigned long long max);
 unsigned long long gcd(unsigned long long x, unsigned long long y);
 unsigned long long lcm(unsigned long long x, unsigned long long y);
 unsigned long long modInverse(unsigned long long e, unsigned long long lam);
-unsigned long long modExp(unsigned long long base,unsigned long long exp,unsigned long long n);
+unsigned long long modExp(unsigned long long base, unsigned long long exp,unsigned long long n);
+unsigned long long brute(unsigned long long c, unsigned long long e, unsigned long long n);
 
 #endif //RSA_H
+
