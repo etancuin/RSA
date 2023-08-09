0105effcb85251c996454345512227364e137394
etancuin <114794458+etancuin@users.noreply.github.com>
2023-08-08 20:41:51 -0700

    Update RSA.cpp

--- RSA.cpp
+++ RSA.cpp
@@ -4,7 +4,8 @@ using namespace std;
 bool isPrime(unsigned long long n) {
   for (size_t i = 2; i <= sqrt(n); ++i)
   {
-    if (n % i == 0) return false;
+    if (n % i == 0) 
+      return false;
   }
   return true;
 }
@@ -37,14 +38,35 @@ unsigned long long modInverse(unsigned long long e, unsigned long long lam)
 {
   for(size_t i = 1; i < lam; ++i)
   {
-    if((i * e) % lam == 1) return i;
+    if((i * e) % lam == 1) 
+      return i;
   }
-  return 
+  return 0;
 }
 
-unsigned long long modExp(unsigned long long base,unsigned long long exp,unsigned long long n)
+unsigned long long modExp(unsigned long long base, unsigned long long exp, unsigned long long n)
 {
   unsigned long long x = 1;
-  for(unsigned long long i = 0; i < exp; ++i) x = (x * base) % n;
+  for(unsigned long long i = 0; i < exp; ++i) 
+    x = (x * base) % n;
   return x;
+}
+
+unsigned long long brute(unsigned long long c, unsigned long long e, unsigned long long n)
+{
+  unsigned long long p = floor(sqrt(n));
+  while(p >= 2)
+  {
+    if(isPrime(p) && n % p == 0)
+      break;
+    --p;
+  }
+  unsigned long long q = n / p;
+  unsigned long long lambda = lcm((p - 1), (q - 1));
+  unsigned long long d = modInverse(e, lambda);
+  if(d != 0)
+  {
+    return modExp(c, d, n); 
+  }
+  return 0;
 }
